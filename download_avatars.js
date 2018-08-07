b009a13d57316abe718f13136b3d04c3930630b6
David Morgan <david.alexandermorgan@gmail.com>
2018-08-07 01:17:44 +0000

    parsing JSON into an object and printing out the url of the contributors' avatar

--- download_avatars.js
+++ download_avatars.js
@@ -13,11 +13,14 @@ function getRepoContributors (repoOwner, repoName, cb){
   };
 
   request(options, function(err, res, body) {
-    cb(err, body);
+    cb(err, JSON.parse(body));
   });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
-  console.log("Result:", result);
+
+  for(var i = 0; i < result.length; i++){
+    console.log("Result:", result[i].avatar_url);
+  }
 });
